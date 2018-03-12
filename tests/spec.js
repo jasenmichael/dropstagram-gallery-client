d7553eb17280a642b613fea41c351881bafc90c9
jasenmichael <jasen@ready-builder.com>
2018-03-12 11:26:57 -0600

    tests that brand logo is correct

--- tests/spec.js
+++ tests/spec.js
@@ -1,6 +1,11 @@
 describe("test dropstagrambox-gallery-client", () => {
-  it("tests the page to see if it...", () => {
+  it("tests the page to see if it loads and filters properly", () => {
     cy.visit("/")
+    cy.wait(3000)
+    // check .brand-logo is "DropstagramboxGallery"
+    cy.get('.brand-logo').should('have.text', 'DropstagramboxGallery')
+
+
 
   })
 })
